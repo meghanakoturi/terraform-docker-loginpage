8d48d2195fef446c28b1a7b5649d20856ae40b48
meghanakoturi <144930899+meghanakoturi@users.noreply.github.com>
2024-07-17 00:34:18 +0530

    Update outputs.tf

--- Login-poc/outputs.tf
+++ Login-poc/outputs.tf
@@ -1,9 +1,5 @@
-output "instance_public_ip" {
-  description = "The public IP address of the EC2 instance"
-  value       = aws_instance.docker_host.public_ip
-}
-
-output "instance_id" {
-  description = "The ID of the EC2 instance"
-  value       = aws_instance.docker_host.id
+# Output the public IP addresses of the instances in the Auto Scaling Group
+output "instance_public_ips" {
+  value       = data.aws_instances.example_instances.public_ips
+  description = "The public IP addresses of the instances in the Auto Scaling Group"
 }
